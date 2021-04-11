bad4c4c6c0ffa9312b3b3bc1032786e2213bc60a
AleksRex <a.bah80@yandex.ru>
2021-04-11 05:27:19 +0300

    Update ssd_start.sh

--- ssd_start.sh
+++ ssd_start.sh
@@ -6,6 +6,6 @@ chmod -R o+x pre-install
 ./pre-install/d_format.sh
 ./pre-install/ssd_btrfs_mount.sh
 ./pre-install/pre-install.sh
-cp -r Archlinux/arch-install-uefi.sh /mnt
-chmod +x /mnt/arch-install-uefi.sh
-arch-chroot /mnt
+#cp -r Archlinux/arch-install-uefi.sh /mnt
+#chmod +x /mnt/arch-install-uefi.sh
+#arch-chroot /mnt
