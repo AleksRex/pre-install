c502339d81fbf0f99a53fc8c6de257b74efb4e2c
AleksRex <a.bah80@yandex.ru>
2021-04-11 05:22:32 +0300

    Update ssd_start.sh

--- ssd_start.sh
+++ ssd_start.sh
@@ -2,10 +2,10 @@
 #!/bin/bash
 
 reflector --verbose --country 'Russia' -l 8 -p http --sort rate
-chmod -R o+x pre-install-linux
-./pre-install-linux/d_format.sh
-./pre-install-linux/ssd_btrfs_mount.sh
-./pre-install-linux/pre-install.sh
+chmod -R o+x pre-install
+./pre-install/d_format.sh
+./pre-install/ssd_btrfs_mount.sh
+./pre-install/pre-install.sh
 cp -r Archlinux/arch-install-uefi.sh /mnt
 chmod +x /mnt/arch-install-uefi.sh
 arch-chroot /mnt
